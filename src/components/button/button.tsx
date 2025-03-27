0c6c9f696f51c9a57a6b3b0d7eecb730df6de068
YonteMoon <monteyoon1998@gmail.com>
2025-03-27 14:46:38 -0700

    started styling button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import "./button.css";
+import React, { ComponentProps } from "react";
+import { TFormatColors } from "@customType/index";
 
 type PropTypes = {
   children: React.ReactNode;
-};
+  colors: TFormatColors;
+} & ComponentProps<"button">;
 
-const Button = ({ children }: PropTypes) => {
-  return <div>{children}</div>;
+const Button = ({ children, colors, ...props }: PropTypes) => {
+  const styledBackgroundColor = `rgb(${colors.background[0]}, ${colors.background[1]}, ${colors.background[2]})`;
+  const styledTextColor = `rgb(${colors.text[0]}, ${colors.text[1]}, ${colors.text[2]})`;
+  return (
+    <button
+      className="button-container"
+      {...props}
+      style={{
+        backgroundColor: styledBackgroundColor,
+        color: styledTextColor,
+      }}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
