7cadf0ae9bec8be1394bf116a67301a55242d24c
YonteMoon <monteyoon1998@gmail.com>
2025-03-27 11:08:14 -0700

    added simple usekeybind hook that sets the next or prev colors

--- src/hooks/useKeybinds.tsx
+++ src/hooks/useKeybinds.tsx
@@ -1,17 +1,30 @@
 import React from "react";
 
-const useKeybinds = () => {
+type PropTypes = {
+  onSpace: () => void;
+  onLeft: () => void;
+  onRight: () => void;
+};
+
+const useKeybinds = ({ onSpace, onLeft, onRight }: PropTypes) => {
   React.useEffect(() => {
     const handleKeyEvents = (event: KeyboardEvent) => {
-      console.log(event.key);
-      if (event.key === " ") {
-        console.log("hello world");
+      switch (event.key) {
+        case " ":
+          onSpace();
+          break;
+        case "ArrowLeft":
+          onLeft();
+          break;
+
+        case "ArrowRight":
+          onRight();
       }
     };
 
     window.addEventListener("keydown", handleKeyEvents);
     return () => window.removeEventListener("keydown", handleKeyEvents);
-  }, []);
+  }, [onLeft, onRight, onSpace]);
 };
 
 export default useKeybinds;
