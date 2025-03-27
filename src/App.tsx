c7186c1e5e73ee374a9ea6294513ae0d1041c6eb
YonteMoon <monteyoon1998@gmail.com>
2025-03-27 14:46:57 -0700

    added state for number of colors shown

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import "./App.css";
+import React, { useEffect } from "react";
 import useColors from "./hooks/useColors";
 import useLocalColors from "./hooks/useLocalColor";
 import useKeybinds from "./hooks/useKeybinds";
 import ColorCard from "./components/color-card/color-card";
+import Button from "./components/button/button";
+import { TColor } from "@customType/index";
 
 function App() {
+  // TODO - Add prettier?
+  // TODO - Add 2-5 color state
+  // TODO - Figure out fonts
+
   const {
     colors,
     toggleColorChange,
@@ -17,18 +24,50 @@ function App() {
   } = useColors();
 
   const [storage, addStorage] = useLocalColors();
-  useKeybinds();
+  const [colorsShown, setColorsShown] = React.useState(5);
+  const [filteredColors, setFilteredColors] = React.useState(colors);
+
+  useEffect(() => {
+    const slicedColors = colors?.slice(0, colorsShown);
+    if (slicedColors) {
+      setFilteredColors(slicedColors);
+    }
+  }, [colors, colorsShown]);
+
+  useKeybinds({
+    onSpace: () => {
+      toggleColorChange();
+    },
+    onLeft: () => {
+      setPrevColor();
+    },
+    onRight: () => {
+      setNextColor();
+    },
+  });
+  const buttonInfo = colors
+    ? colors[1]
+    : {
+        background: [235, 198, 126] as TColor,
+        text: [235, 198, 126] as TColor,
+      };
 
   return (
     <div className="app-container">
-      <div className="grid-container ">
-        {colors?.map((color, index) => {
+      <div
+        className="grid-container"
+        style={{
+          gridTemplateRows: `repeat(${colorsShown}, minmax(0, 1fr))`,
+        }}
+      >
+        {filteredColors?.map((color, index) => {
           return <ColorCard colors={color} key={index} />;
         })}
       </div>
-      <div className="button-random">
-        <button
+      <div className="top-navigator">
+        <Button
           disabled={storage?.find((s) => s === colors) ? true : false}
+          colors={buttonInfo}
           onClick={() => {
             if (colors) {
               addStorage(colors);
@@ -36,16 +75,36 @@ function App() {
           }}
         >
           Like
-        </button>
-        <button onClick={setPrevColor} disabled={isFirst}>
+        </Button>
+        <Button colors={buttonInfo} onClick={() => setColorsShown(2)}>
+          2
+        </Button>
+        <Button colors={buttonInfo} onClick={() => setColorsShown(3)}>
+          3
+        </Button>
+        <Button colors={buttonInfo} onClick={() => setColorsShown(4)}>
+          4
+        </Button>
+        <Button colors={buttonInfo} onClick={() => setColorsShown(5)}>
+          5
+        </Button>
+
+        <Button
+          onClick={toggleColorChange}
+          disabled={isGenerating}
+          colors={buttonInfo}
+          className="generate-button button-container"
+        >
+          {isGenerating ? "Generating" : "Generate"}
+        </Button>
+      </div>
+      <div className="buttom-navigator">
+        <Button onClick={setPrevColor} disabled={isFirst} colors={buttonInfo}>
           Previous
-        </button>
-        <button onClick={setNextColor} disabled={isLast}>
+        </Button>
+        <Button onClick={setNextColor} disabled={isLast} colors={buttonInfo}>
           Next
-        </button>
-        <button onClick={toggleColorChange} disabled={isGenerating}>
-          {isGenerating ? "Generating" : "Click ME"}
-        </button>
+        </Button>
       </div>
     </div>
   );
